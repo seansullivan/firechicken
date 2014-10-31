8091bc13cd1e462da2d436e3373258f0935e9d6b
Sean Sullivan <ssullivan@cbtnuggets.com>
2014-10-31 14:23:55 -0700

    Update nodemailer and transport for AWS

--- mail.js
+++ mail.js
@@ -5,11 +5,6 @@ var nodemailer = require('nodemailer'),
 var mail = function (config, recipient, stat, val) {
     this.config = config;
 
-    nodemailer.SMTP = {
-        host: config.host,
-        port: config.port
-    };
-
     this.recipient = recipient;
     this.subject = "Erorr reported for: "+stat;
 
@@ -20,24 +15,35 @@ var mail = function (config, recipient, stat, val) {
 mail.prototype = new events.EventEmitter();
 
 mail.prototype.send = function () {
-    var self = this;
-
-    nodemailer.send_mail(
-        {
-            sender: this.config.sender,
-            to:this.recipient,
-            subject:this.subject,
-            body:this.body
+    var self = this,
+        mailOptions = {
+            from: this.config.sender,
+            to: this.recipient,
+            subject: this.subject,
+            html: this.body
         },
-        function(error, success){
-            if(success) {
-                cli.debug("Sent email to " + self.recipient);
-            }
-            else {
-                cli.error("Sending email failed: " + error.message);
-            }
+        transporter = null;
+
+    if(this.config.aws) {
+        transporter = nodemailer.createTransport("SES", {
+            AWSAccessKeyID: this.config.aws.key,
+            AWSSecretKey: this.config.aws.secret
+        });
+    }
+    else {
+        transporter = nodemailer.createTransport();
+    }
+
+    transporter.sendMail(mailOptions, function(error, response) {
+        if(error) {
+            cli.error(error);
         }
-    );
+        else {
+            cli.debug("Sent email to " + self.recipient);
+        }
+
+        transporter.close(); // shut down the connection pool, no more messages
+    });
 };
 
 module.exports = mail;
